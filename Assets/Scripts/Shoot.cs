36919336691590fc7b9e83c896368cf3948abdde
mansgrundberg <mans.grundberg@gmail.com>
2020-05-11 18:09:57 +0200

    La till fire rate

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,26 +9,24 @@ public class Shoot : MonoBehaviour {
     [SerializeField] private GameObject bullet;
     [SerializeField] private Transform gun;
     [SerializeField] private float bulletVel = 5;
-    
+    [SerializeField] private float fireRate;
+    private float lastShot = 0;
+
 
 
     // Update is called once per frame
     void Update() {
         if (Input.GetButtonDown("Fire1")) {
-
-            // Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-            // if (Physics.Raycast(ray, out RaycastHit hit, 100)) {
-            //     Debug.LogFormat(hit.collider.gameObject.name);
-            // hit.transform.SendMessage("Fire", SendMessageOptions.DontRequireReceiver);
-            // }
-
-            var go = Instantiate(bullet, gun.position, gun.rotation);
-            go.GetComponent<Rigidbody>().velocity = go.transform.forward * bulletVel;
-            Destroy(go, 3);
+            Fire();
         }
     }
 
-    private void OnTriggerEnter(Collider other) {
-        Debug.Log("Träff");
+    private void Fire() {
+        if (Time.time > fireRate + lastShot) {
+            var go = Instantiate(bullet, gun.position, gun.rotation);
+            go.GetComponent<Rigidbody>().velocity = go.transform.forward * bulletVel;
+            Destroy(go, 3);
+            lastShot = Time.time;
+        }
     }
 }
