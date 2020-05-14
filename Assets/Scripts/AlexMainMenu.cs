c2403565cec7cf088738cd21c16bb8b6970bc95c
Alexander Libot <libot@mail.com>
2020-05-15 00:16:02 +0200

    Added main menu

---
+++ Assets/Scripts/AlexMainMenu.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AlexMainMenu : MonoBehaviour
+{
+
+    [SerializeField]
+    private string gameSceneName;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
