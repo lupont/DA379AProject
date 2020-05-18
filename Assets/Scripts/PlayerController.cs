1f89f55843153163037408b218323fa38eec47ba
mansgrundberg <mans.grundberg@gmail.com>
2020-05-18 16:20:05 +0200

    Hej

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,31 +4,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public CharacterController controller;
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private Animator animator;
 
     public float walkSpeed = 10;
     public float runSpeed = 15;
     public float gravity;
     public float jumpForce;
-
     private float speed;
     private float y_velocity;
-
     private Vector3 movement = Vector3.zero;
 
-    // Start is called before the first frame update
-    void Start() {
-      
-    }
 
     // Update is called once per frame
     void Update() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        checkRun();
         
+        checkRun();
+
         movement = (transform.right * moveHorizontal + moveVertical * transform.forward);
         movement *= speed;
+
+        animator.SetFloat("DirectionX", moveHorizontal);
+        animator.SetFloat("DirectionY", moveVertical);
+        animator.SetBool("Moving", movement.sqrMagnitude > 0);
 
         if (controller.isGrounded && Input.GetKey(KeyCode.Space)) {
             y_velocity = jumpForce;
