a7306986fc02a14417229fac71c0b55879365884
mansgrundberg <mans.grundberg@gmail.com>
2020-05-22 02:14:19 +0200

    Multiplayer/network, playerui och mer gutt

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class BulletScript : NetworkBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.transform.CompareTag("Player")) {
+            CmdPlayerShot(collision.transform.name);
+        }
+    }
+
+    void CmdPlayerShot(string id) {
+        Debug.Log(id + "has been shot");
+        GameObject.Find(id).SendMessage("DealDamage");
+    }
+
+}
