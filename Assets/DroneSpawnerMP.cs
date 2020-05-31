bc76ad8c95126d37b9532a7919f4cea5613d711d
mansgrundberg <mans.grundberg@gmail.com>
2020-05-31 15:14:22 +0200

    läget

--- Assets/DroneSpawnerMP.cs
+++ Assets/DroneSpawnerMP.cs
@@ -5,6 +5,8 @@ using Mirror;
 
 public class DroneSpawnerMP : NetworkBehaviour {
     [SerializeField] private GameObject drone;
+    private Vector3 pos = new Vector3(50, 5, -16);
+
     // Start is called before the first frame update
     void Start() {
         CmdSpawnDrone();
@@ -17,7 +19,7 @@ public class DroneSpawnerMP : NetworkBehaviour {
 
     [Command]
     void CmdSpawnDrone() {
-        var go = Instantiate(drone, gameObject.transform.position, gameObject.transform.rotation);
+        var go = Instantiate(drone, pos, new Quaternion(0,0,0,0));
         NetworkServer.Spawn(go);
         Debug.Log("Spawning drone");
     }
---
+++ Assets/LoadSinglePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSinglePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LoadScene() {
+        SceneManager.LoadScene("SinglePlayerScene", LoadSceneMode.Single);
+    }
+   
+}
