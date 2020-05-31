e06fa462f6567a4c2303c4a357e53c8c19acba8f
mansgrundberg <mans.grundberg@gmail.com>
2020-05-31 14:32:33 +0200

    hej

---
+++ Assets/DroneSpawnerMP.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class DroneSpawnerMP : NetworkBehaviour {
+    [SerializeField] private GameObject drone;
+    // Start is called before the first frame update
+    void Start() {
+        CmdSpawnDrone();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    [Command]
+
+    void CmdSpawnDrone() {
+        var go = Instantiate(drone, gameObject.transform.position, gameObject.transform.rotation);
+        NetworkServer.Spawn(go);
+        Debug.Log("Spawning drone");
+    }
+
+}
